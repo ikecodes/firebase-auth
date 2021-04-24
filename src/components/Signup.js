a9388f1bece53cfa4ff44212b7936c1929d0ca21
onuorah ike <jsxike@gmail.com>
2021-04-24 18:44:48 +0100

    auth context init

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,10 +1,17 @@
 import React, { useRef } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 const Signup = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <Card>
